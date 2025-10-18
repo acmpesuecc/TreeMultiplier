d3582a7b0042169d084b8f6d3df0b1be2d3cd8e9
Tanmay Sankolli <148807060+tvs-tanmay@users.noreply.github.com>
2025-10-18 10:13:24 +0530

    Refactor top_level_multiplier module implementation
    Had accidentally commited reduction.sv here

--- Wallace6x6/top_level_multiplier.sv
+++ Wallace6x6/top_level_multiplier.sv
@@ -1,87 +1,47 @@
 `timescale 1ns / 1ps
 
-module wallace_tree_reduction(
-	input logic [5:0] A, B,
-	output logic [11:0] R1, R2);
+module top_level_multiplier(A, B, P);
+	input logic [5:0] A, B;
+	output logic [11:0] P;
+	
+	logic [11:0] R3, R4;
+	wallace_tree_reduction wtr(.a(A), .b(B), .r1(R1), .r2(R2));
+	final_adder fa(.a(R1), .b(R2), .s(P));
 
-			logic [13:0]S_FA, C_FA;
-            logic [14:0]S_HA, C_HA;
+	wallace_tree_reduction wtr(.A(A), .B(B), .R1(R3), .R2(R4));
 
-            HA ha1(B[2]&A[0], B[1]&A[1], S_HA[0],C_HA[0]);
+    final_adder fa1(R3[1], R4[1], S_P[0], C_P[0]);
 
-			FA fa1(B[3]&A[0], B[2]&A[1], B[1]&A[2], S_FA[0], C_FA[0]);
+    final_adder fa2(R3[2], R4[2], S_P[1], C_P[1]);
 
-			FA fa2(B[4]&A[0], B[3]&A[1], B[2]&A[2], S_FA[1], C_FA[1]);
+    final_adder fa3(R3[3], R4[3], S_P[2], C_P[2]);
 
-            HA ha2(B[1]&A[3], B[0]&A[4], S_HA[1], C_HA[1]);
+    final_adder fa4(R3[4], R4[4], S_P[3], C_P[3]);
 
-			FA fa3(B[5]&A[0], B[4]&A[1], B[3]&A[2], S_FA[2], C_FA[3]);
+    final_adder fa5(R3[5], R4[5], S_P[4], C_P[4]);
 
-			FA fa4(B[2]&A[3], B[1]&A[4], B[0]&A[5], S_FA[3], C_FA[3]);
+    final_adder fa6(R3[6], R4[6], S_P[5], C_P[5]);
 
-			FA fa5(B[5]&A[1], B[4]&A[2], B[3]&A[3], S_FA[4], C_FA[4]);
+    final_adder fa7(R3[7], R4[7], S_P[6], C_P[6]);
 
-			FA fa6(B[5]&A[2], B[4]&A[3], B[3]&A[4], S_FA[5], C_FA[5]);
+    final_adder fa8(R3[8], R4[8], S_P[7], C_P[7]);
 
-			HA ha4(B[5]&A[3], B[4]&A[4], S_HA[3], C_HA[3]);
+	final_adder fa9(R3[9], R4[9], S_P[0], C_P[0]);
 
-			HA ha5(C_HA[0], S_FA[0], S_HA[1], C_HA[1]);
+	final_adder fa10(R3[10], R4[10], S_P[0], C_P[0]);
 
-            FA fa7(C_FA[0], S_FA[1], S_HA[1], S_FA[6], C_FA[6]);
-
-            FA fa8(C_FA[1], C_HA[1], S_FA[2], S_FA[7], C_FA[7]);
-
-            FA fa9(C_FA[2], C_FA[3], S_FA[4], S_FA[8], C_FA[8]);
-
-			FA fa10(C_FA[4], C_HA[2], S_FA[5], S_FA[9], C_FA[9]);
-
-			HA ha6(C_FA[5], S_HA[3], S_HA[5], C_HA[5]);
-
-			HA ha7(C_HA[3], B[5]&A[4], S_HA[6], C_HA[6]);
-
-			HA ha8(C_FA[6], S_FA[7], S_HA[7], C_HA[7]);
-
-			HA ha9(C_FA[7], S_FA[8], S_HA[8], C_HA[8]);
-
-			HA ha10(C_FA[8], S_FA[9], S_HA[9], C_HA[9]);
-
-			HA ha11(C_FA[9], S_HA[5], S_HA[10], C_HA[10]);
-
-			HA ha12(C_HA[5], S_HA[6], S_HA[11], C_HA[11]);
-
-			HA ha13(C_HA[7], S_HA[8], S_HA[12], C_HA[12]);
-
-			FA fa11(C_HA[8], S_HA[9], B[2]&A[5], S_FA[10], C_FA[10]);
-
-			FA fa12(C_HA[9], S_HA[10], B[3]&A[5], S_FA[11], C_FA[11]);
-
-			FA fa13(C_HA[10], S_HA[11], B[4]&A[5], S_FA[12], C_FA[12]);
-
-			HA ha14(C_HA[11], B[5]&A[5], S_HA[13], C_HA[13]);
-
-            assign R1[0]=B[0]&A[0];
-			assign R1[1]=B[1]&A[0];
-			assign R1[2]=S_HA[0];
-			assign R1[3]=S_HA[4];
-			assign R1[4]=C_HA[4];
-			assign R1[5]=S_HA[8];
-			assign R1[6]=S_HA[13];
-			assign R1[7]=C_HA[13];
-			assign R1[8]=C_FA[11];
-			assign R1[9]=C_FA[12];
-			assign R1[10]=C_FA[13];
-			assign R1[11]=1'b0;
-
-			assign R2[0]=1'b0;
-            assign R2[1]=B[0]&A[1];
-			assign R2[2]=B[0]&A[2];
-			assign R2[3]=B[0]&A[3];
-			assign R2[4]=S_FA[7];
-			assign R2[5]=S_FA[4];
-			assign R2[6]=S_HA[3];
-			assign R2[7]=S_FA[11];
-			assign R2[8]=S_FA[12];
-			assign R2[9]=S_FA[13];
-			assign R2[10]=S_HA[14];
-			assign R2[11]=C_HA[14];
-endmodule
+    assign P[0]=R3[0];
+    assign P[1]=S_P[0];
+    assign P[2]=S_P[1];
+    assign P[3]=S_P[2];
+    assign P[4]=S_P[3];
+    assign P[5]=S_P[4];
+    assign P[6]=S_P[5];
+    assign P[7]=S_P[6];
+    assign P[8]=S_P[7];
+	assign P[9]=S_P[8];
+	assign P[10]=S_P[9];
+	assign P[11]=S_P[10];
+    assign P[12]=R4[11];  
+	
+endmodule 
